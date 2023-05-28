bb1eca93163baa879ad87dd61e1136d37ebc758f
merinnnn <123597118+Merin02@users.noreply.github.com>
2023-05-28 11:11:24 +0100

    Added findUnknownUnit

--- src/Solver.java
+++ src/Solver.java
@@ -40,6 +40,26 @@ public class Solver {
     }
 
 
+    public int findUnit(int[] partialAssignment, int[] clause){
+        int count = 0;
+        int unknownLiteral = 0;
+        
+        for (int i=0; i<clause.length; i++){
+            int literal = Math.abs(clause[i]);
+            if (partialAssignment[literal] == Integer.signum(clause[i])){
+                return 0;
+            } else if (partialAssignment[literal] == 0) {
+                unknownLiteral = clause[i];
+                count++;
+
+                if (count > 1){
+                    return 0;
+                }
+            }
+        }
+
+        return count == 1 ? unknownLiteral : 0;
+    }
 
 
 
